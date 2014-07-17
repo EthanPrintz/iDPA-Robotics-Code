11d4a5faebd430a6d28170df38bae70825603151
Ethan Printz <ethanprintz@gmail.com>
2014-07-16 17:46:11 -0700

    BUGFIX | Adjusted goalie return values
    The goalie was not reseting to the same place in front of the goal.

--- Tank.c
+++ Tank.c
@@ -32,10 +32,16 @@ task bumper{while(true){
 	motor[mleft]=100;wait1Msec(1100);end();StopTask(bumper);
 }}
 task goalie{while(true){
-	 wait1Msec(9000);motor[mleft]=127;motor[mright]=127;wait1Msec(750);motor[mleft]=-127;motor[mright]=-127;wait1Msec(750);
-	 end();
+	 wait1Msec(9029);motor[mleft]=127;motor[mright]=127;wait1Msec(750);motor[mleft]=-127;motor[mright]=-127;wait1Msec(790);end();
+	 wait1Msec(5276);motor[mleft]=127;motor[mright]=127;wait1Msec(750);motor[mleft]=-127;motor[mright]=-127;wait1Msec(790);end();
+	 wait1Msec(2565);motor[mleft]=127;motor[mright]=127;wait1Msec(750);motor[mleft]=-127;motor[mright]=-127;wait1Msec(790);end();
+	 wait1Msec(8851);motor[mleft]=127;motor[mright]=127;wait1Msec(750);motor[mleft]=-127;motor[mright]=-127;wait1Msec(790);end();
+	 wait1Msec(4042);motor[mleft]=127;motor[mright]=127;wait1Msec(750);motor[mleft]=-127;motor[mright]=-127;wait1Msec(790);end();
+	 wait1Msec(7615);motor[mleft]=127;motor[mright]=127;wait1Msec(750);motor[mleft]=-127;motor[mright]=-127;wait1Msec(790);end();
+	 wait1Msec(3315);motor[mleft]=127;motor[mright]=127;wait1Msec(750);motor[mleft]=-127;motor[mright]=-127;wait1Msec(790);end();
 }}
 task arcadekicker{while(true){
+
 	motor[mleft]  = (vexRT[Ch2] + vexRT[Ch1]);
 	motor[mright] = (vexRT[Ch2] - vexRT[Ch1]);
 	motor[leg] = (vexRT[Ch4] + vexRT[Ch3]);
